ab7749c8b625e5f593e2776a9664e2c55cec35fe
suganth <112330202+Suganth01@users.noreply.github.com>
2024-08-31 12:12:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function icon1(){
+    window.open('https://www.linkedin.com/in/suganth-ss/','_blank');
+}
+function icon2(){
+    window.open("https://github.com/Suganth01",'_blank')
+}
+function icon3(){
+    window.open("https://www.instagram.com/_.suganth._/?hl=en",'_blank');
+}
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
